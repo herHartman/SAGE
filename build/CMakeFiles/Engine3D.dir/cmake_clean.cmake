bde42f442579301c8a84630b47945fbb2b7bbf62
Гартман Артем <gartman.a@orionnet.ru>
2022-01-22 12:34:20 +0700

    init commit

---
+++ build/CMakeFiles/Engine3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine3D.dir/main.cpp.o"
+  "Engine3D"
+  "Engine3D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
